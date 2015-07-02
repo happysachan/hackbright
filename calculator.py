155141da4a9af2484a1eda30e7cf3bd444dc95e5
Hackbright Student <info@hackbrightacademy.com>
2015-07-02 12:40:32 -0700

    fixed for 2 inputs

--- calculator.py
+++ calculator.py
@@ -1,25 +1,30 @@
-from arithmetic import add, subtract, multiply, divide, square, power, mod
+from arithmetic import *
 
 while True:
     calc = raw_input("> ")
     inputs = calc.split(" ")
-    len(inputs)
-    print inputs
+    #print len(inputs)
 
-    i1 = int(inputs[1])
-    i2 = int(inputs[2])
+    if len(inputs) == 2:
+        i1 = int(inputs[1])
 
-    if inputs[0] == '+':
-        print add(i1,i2)
-    if inputs[0] == '-':
-        print subtract(i1,i2)
-    if inputs[0] == '*':
-        print multiply(i1,i2)   
-    if inputs[0] == '/':
-        print divide(i1,i2)   
-    if inputs[0] == 'square':
-        print square(i1)   
-    if inputs[0] == 'pow':
-        print power(i1,i2)
-    if inputs[0] == '%':
-        print mod(i1,i2)      
+        if inputs[0] == 'square':
+            print square(i1)  
+        if inputs[0] == 'cube':
+            print cube(i1)
+    else:
+        i1 = int(inputs[1])
+        i2 = int(inputs[2])
+
+        if inputs[0] == '+':
+            print add(i1,i2)
+        if inputs[0] == '-':
+            print subtract(i1,i2)
+        if inputs[0] == '*':
+            print multiply(i1,i2)   
+        if inputs[0] == '/':
+            print divide(i1,i2)   
+        if inputs[0] == 'pow':
+            print power(i1,i2)
+        if inputs[0] == '%':
+            print mod(i1,i2)      
